8ec23d8430d5ab2dee1a1e13edcfbbfcf67684ca
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2018-08-05 15:04:34 +0200

    g1

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,3 +1,23 @@
 (function () {
     const gameElement = document.createElement("div");
+
+    const panelElement = document.createElement("div");
+
+    const arenaElement = document.createElement("div");
+
+    const moleElement = document.createElement("div");
+
+    gameElement.classList.add("game");
+    panelElement.classList.add("panel");
+    arenaElement.classList.add("arena");
+    moleElement.classList.add("mole");
+
+    gameElement.appendChild(panelElement);
+    gameElement.appendChild(arenaElement);
+
+    arenaElement.appendChild(moleElement);
+
+
+
+    document.body.appendChild(gameElement);
 }());
