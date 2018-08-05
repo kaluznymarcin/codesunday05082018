6fbef1b87744399c582b102e7b4a5f5ad88c86c6
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2018-08-05 16:11:53 +0200

    g3

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,5 +1,6 @@
 (function () {
     let score = 0;
+    let intervalID;
 
     const gameElement = document.createElement("div");
 
@@ -19,7 +20,18 @@
 
     arenaElement.appendChild(moleElement);
 
+    function setMolePosition() {
+        moleElement.style.top = (Math.round(Math.random() * 9) * 50) + "px";
+        moleElement.style.left = (Math.round(Math.random() * 9) * 50) + "px";
+    }
+
+    function startInterval() {
+        clearInterval(intervalID);
+        intervalID = setInterval(setMolePosition, 1500);
+    }
+
     function callback(e) {
+        e.stopPropagation();
         const clickedElement = e.target;
         if (
             clickedElement
@@ -27,8 +39,10 @@
             .contains("mole")
         ) {
             score += 1;
-            // score = score + 1;
-            console.log(score);
+            panelElement.innerHTML = score;
+
+            setMolePosition();
+            startInterval();
         }
     }
 
@@ -39,4 +53,5 @@
     );
 
     document.body.appendChild(gameElement);
+    startInterval();
 }());
